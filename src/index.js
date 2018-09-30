20462f345d1d8a6e67db5a76a71bf18a4f6e5a44
alonko <kodneralon@gmail.com>
2018-09-30 20:57:38 +1300

    rmoved

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom';
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyA_EeLIKEwEdw0RKU8ap5ZH2ZRF2g8xp2k';
+const API_KEY = '';
 
 const App = () => {
     return (
